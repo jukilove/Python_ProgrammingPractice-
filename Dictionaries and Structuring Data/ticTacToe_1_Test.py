f6f3441b73ab6eb232d03d36cc818b57e8a90fc5
Elizabeth Owolabi <jukilove.c@gmail.com>
2018-06-13 17:44:02 -0400

    add TicTacToe game files

---
+++ Dictionaries and Structuring Data/ticTacToe_1_Test.py
@@ -0,0 +1,58 @@
+#Using Data Structures to Model Real-World Things.
+#A Tic-Tac-Toe Board
+#To move, the Tic-Tac-Toe Board space are called:
+#top-L | top-M | top-R
+#------+-------+------
+#mid-L | mid-M | mid-R
+#------+-------+------
+#low-L | low-M | low-R
+
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+           
+def printTTTBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+def introTTTBoard():
+    print('Welcome to Python Tic-Tac-Toe Board.\nHere are some few instructions to the game.')
+    print('- The board is display as')
+    printTTTBoard(theBoard)
+    print('- There are nine space that can each contain an X, an O, or a blank.')
+    print('- To move on which space, use these keys word: \n' +
+          'top-L | top-M | top-R \n' + '------+-------+------\n' +
+          'mid-L | mid-M | mid-R \n' + '------+-------+------\n' +
+          'low-L | low-M | low-R \n' + '######################################')
+    
+introTTTBoard()
+
+userTurn = 'X'
+
+for i in range(9):
+    printTTTBoard(theBoard)
+        print("%s's turn" % User + '. Move on which space?')
+        move = input()
+        if theBoard[move] == 'X' or theBoard[move] == 'O':
+            print('>>> Already taken! Sorry.')
+        else:
+            theBoard[move] = User
+            if User == 'X':
+                print('```````````````````````````````````````````````')
+                print('- To move on which space, use these keys word: \n' +
+                      'top-L | top-M | top-R \n' + '------+-------+------\n' +
+                      'mid-L | mid-M | mid-R \n' + '------+-------+------\n' +
+                      'low-L | low-M | low-R \n' + '```````````````````````````````````````````````')
+                User = 'O'
+            else:
+                print('```````````````````````````````````````````````')
+                print('- To move on which space, use these keys word: \n' +
+                      'top-L | top-M | top-R \n' + '------+-------+------\n' +
+                      'mid-L | mid-M | mid-R \n' + '------+-------+------\n' +
+                      'low-L | low-M | low-R \n' + '```````````````````````````````````````````````')
+                User = 'X'
+                
+printTTTBoard(theBoard)
