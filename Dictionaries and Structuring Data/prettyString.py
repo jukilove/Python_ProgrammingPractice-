93beaee5920fbcb5fdc7fe5e568471a39aceba59
Elizabeth Owolabi <jukilove.c@gmail.com>
2018-06-13 14:22:31 -0400

    add prettyString file

---
+++ Dictionaries and Structuring Data/prettyString.py
@@ -0,0 +1,15 @@
+#prettyString - uses (pprint&pformat) python module to display a cleaner
+# items in a dictionary
+
+import pprint
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count)
+print('####')
+print(pprint.pformat(count))
